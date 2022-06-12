5d03801e6048e442e9c3e87920760e6b9033a0fd
Šimon Líman <vrt2@seznam.cz>
2022-06-13 00:30:41 +0200

    up

--- api.py
+++ api.py
@@ -3,21 +3,29 @@
 
 from __future__ import annotations
 
+import logging
 from typing import Union
 
 from fastapi import FastAPI
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%d.%m.%Y %H:%M:%S",
+    format="[%(asctime)s] %(levelname)s [%(name)s:%(module)s - %(funcName)s:%(lineno)s] %(message)s")
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    logger.info("yolo")
+    return {"Hello": "WorldZOOP "}
 
 
 @app.get("/items/{item_id}")
 def read_item(
         item_id: int,
         q: Union[str, None] = None
-        ):
+):
     return {"item_id": item_id, "q": q}
